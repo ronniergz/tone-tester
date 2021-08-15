1139e999fbe89f4014c5c72a802c1bd07974eeda
Ronnie Rodriguez <rlrodrig88@gmail>
2021-08-15 13:25:02 -0500

    switched to Web Audio API

--- app.js
+++ app.js
@@ -1,46 +1,32 @@
-const button40 = document.querySelector(".button-40");
-const button60 = document.querySelector(".button-60");
-const button80 = document.querySelector(".button-80");
-const button100 = document.querySelector(".button-100");
-const buttonSweep = document.querySelector(".button-sweep");
+const buttonPlay = document.querySelector(".button-play");
+const buttonPulse = document.querySelector(".button-pulse");
 const buttonStop = document.querySelector(".stop");
-const frequency = document.querySelector(".active-freq");
+const freq = document.querySelector(".freq");
+const activeFreq = document.querySelector(".active-freq");
 
-// Select and Play Audio Files
-const play = (e) => {
-  stop();
-  const audio = document.querySelector(`.tone-${e.target.dataset.key}`);
-  audio.play();
-  if (e.target.dataset.key === "sweep") {
-    sweep();
-  } else frequency.innerHTML = `${e.target.dataset.key}Hz`;
+// Play frequency
+const play = () => {
+  if (playing === true) return;
+  osc = audioContext.createOscillator();
+  osc.type = "sine";
+  osc.frequency.value = freq.value;
+  osc.connect(audioContext.destination);
+  osc.start();
+  activeFreq.innerHTML = `${osc.frequency.value}Hz`;
+  playing = true;
+  return osc;
 };
 
-const sweep = () => {
-  let frequencyCurrent = 40,
-    interval;
-  const showFrequency = () => {
-    frequencyCurrent = frequencyCurrent + 1.2;
-    frequency.innerHTML = Math.round(frequencyCurrent);
-    if (frequencyCurrent >= 160) clearInterval(interval);
-  };
-  interval = setInterval(showFrequency, 200);
-};
-
-// Stop Audio Files
-const stop = (e) => {
-  frequency.innerHTML = "";
-  const audioAll = document.querySelectorAll("audio");
-  audioAll.forEach((e) => {
-    e.pause();
-    e.currentTime = 0;
-  });
-};
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+var osc = null;
+var playing = false;
 
 // Listen for Button Clicks
-button40.addEventListener("click", play);
-button60.addEventListener("click", play);
-button80.addEventListener("click", play);
-button100.addEventListener("click", play);
-buttonSweep.addEventListener("click", play);
-buttonStop.addEventListener("click", stop);
+buttonPlay.addEventListener("click", play);
+buttonPulse.addEventListener("click", play);
+buttonStop.addEventListener("click", (e) => {
+  e.preventDefault();
+  osc.stop();
+  activeFreq.innerHTML = ``;
+  playing = false;
+});
