9d17452ac2e19a9b3e59e029e2e1729396d3789f
Ronnie Rodriguez <rlrodrig88@gmail>
2021-08-15 18:24:26 -0500

    added slider functionalitly and items for heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const buttonPlay = document.querySelector(".button-play");
 const buttonPulse = document.querySelector(".button-pulse");
 const buttonStop = document.querySelector(".stop");
 const freq = document.querySelector(".freq");
+const freqSlider = document.querySelector("#freq-slider");
 const activeFreq = document.querySelector(".active-freq");
 
 // Play frequency
@@ -14,25 +15,27 @@ const play = () => {
   osc.connect(gain);
   gain.connect(audioContext.destination);
   osc.start(0);
-  activeFreq.innerHTML = `${osc.frequency.value}Hz`;
   playing = true;
   return osc;
 };
 
 // Play frequency on mouse or keydown only
-const pulse = () => {
-  play();
-};
+const pulse = () => play();
 
 const stop = (e) => {
   e.preventDefault();
+  if (gain === null) return;
   gain.gain.exponentialRampToValueAtTime(
     0.00001,
     audioContext.currentTime + 0.04
   );
-  //osc.stop();
-  activeFreq.innerHTML = ``;
   playing = false;
+};
+
+const updateFreq = () => {
+  freq.value = freqSlider.value;
+  if (osc === null) return;
+  osc.frequency.value = freq.value;
 };
 
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -40,8 +43,11 @@ var osc = null;
 var gain = null;
 var playing = false;
 
+freq.value = freqSlider.value = 300; // start application at 300Hz
+
 // Listen for Button Clicks
 buttonPlay.addEventListener("click", play);
 buttonPulse.addEventListener("mousedown", pulse);
 buttonPulse.addEventListener("mouseup", stop);
 buttonStop.addEventListener("click", stop);
+freqSlider.addEventListener("input", updateFreq);
